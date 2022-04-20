1474abb8d6f9480fd6a8f0883a9e1d1261babfeb
ArvaBayu <arvabayususanto@gmail.com>
2022-04-20 10:30:39 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\NilaiController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('/mahasiswa', MahasiswaController::class);
+
+Route::get('search', [MahasiswaController::class,'search'])->name('search');
+Route::get('/mahasiswa/nilai/{mahasiswa}', [NilaiController::class, 'index']);
